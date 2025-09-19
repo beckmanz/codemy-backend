1401e31eddd2cce8209ae87a5419f747676bd62c
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-19 20:35:26 -0300

    feat: configura scalar para documentação e teste da api

--- codemy-backend/Program.cs
+++ codemy-backend/Program.cs
@@ -1,5 +1,10 @@
 using codemy_backend;
+using codemy_backend.Middlewares;
+using codemy_backend.Repositories.User;
+using codemy_backend.Services.Auth;
+using codemy_backend.Services.User;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddScoped<IAuthInterface, AuthService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -20,10 +28,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
