f17114565fe576f4390543f9485bbc4b0f533dd7
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-20 22:33:55 -0300

    feat: cria entidade de resposta para padronizar as respostas da api

---
+++ codemy-backend/Models/Dtos/Response/ResponseModel.cs
@@ -0,0 +1,23 @@
+﻿namespace codemy_backend.Models.Dtos.Response;
+
+public abstract class ResponseModel
+{
+    protected ResponseModel(bool isSuccess, string message)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+    public bool IsSuccess { get; private set; }
+    public string Message { get; private set; }
+}
+
+public class ResponseData<T> : ResponseModel
+{
+    public ResponseData(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
+    {
+        Data = data;
+    }
+    public T? Data { get; set; }
+    public static ResponseData<T> Success(T data, string message = "Operação realizada com sucesso.") => new (data, true, message);
+    
+}
