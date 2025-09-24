ff51c1e605496978437ad97b337d454c97c2e0a5
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-24 13:49:09 -0300

    feat: cria SignUpRequestDto

---
+++ codemy-backend/Models/Dtos/Request/SignUpRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using codemy_backend.Models.Dtos.Response;
+
+namespace codemy_backend.Models.Dtos.Request;
+
+public class SignUpRequestDto
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public UserRole Role { get; set; }
+    public static implicit operator UserModel(SignUpRequestDto dto)
+    {
+        return new UserModel()
+        {
+            Name = dto.Name,
+            Email = dto.Email,
+            PasswordHash = dto.Password,
+            Role = dto.Role
+        };
+    }
+}
