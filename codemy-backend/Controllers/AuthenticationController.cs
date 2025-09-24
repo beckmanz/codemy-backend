60008fb81ed7248f706242e5d420b0ff9e97fda5
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-24 13:55:13 -0300

    feat: cria AuthenticationController com endpoint de SignUp

---
+++ codemy-backend/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using codemy_backend.Models.Dtos;
+using codemy_backend.Models.Dtos.Request;
+using codemy_backend.Models.Dtos.Response;
+using codemy_backend.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codemy_backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthInterface _authInterface;
+
+    public AuthenticationController(IAuthInterface authInterface)
+    {
+        _authInterface = authInterface;
+    }
+    
+    [HttpPost("signup")]
+    public async Task<ActionResult<ResponseModel>> SignUp(SignUpRequestDto dto)
+    {
+        var response = await _authInterface.SignUp(dto);
+        return Ok(response);
+    }
+}
