6b12ecf2c766e9162bb7e8c5d488a6d22d9d526c
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-24 13:53:34 -0300

    feat: cria serviço de Authenticação e método SignUp

---
+++ codemy-backend/Services/Auth/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using codemy_backend.Exceptions;
+using codemy_backend.Models.Dtos;
+using codemy_backend.Models.Dtos.Request;
+using codemy_backend.Models.Dtos.Response;
+using codemy_backend.Repositories.User;
+using codemy_backend.Services.Token;
+
+namespace codemy_backend.Services.Auth;
+
+public class AuthService : IAuthInterface
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ITokenInterface  _tokenInterface;
+
+    public AuthService(IUserRepository userRepository, ITokenInterface tokenInterface)
+    {
+        _userRepository = userRepository;
+        _tokenInterface = tokenInterface;
+    }
+
+    public async Task<ResponseModel> SignUp(SignUpRequestDto signUpRequestDto)
+    {
+        var userEmailExist = await _userRepository.GetByEmailAsync(signUpRequestDto.Email);
+        if (userEmailExist is not null)
+        {
+            throw new ConflictException("Email já existe!");
+        }
+        UserModel newUser = signUpRequestDto;
+        var user = await _userRepository.CreateUserAsync(newUser);
+        
+        var token = _tokenInterface.GetAcessToken(user.Id, user.Name, user.Role);
+
+        AuthResponseDto authDto = user;
+        authDto.Token = token;
+        
+        var response = ResponseData<AuthResponseDto>.Success(authDto);
+        return response;
+    }
+}
---
+++ codemy-backend/Services/Auth/IAuthInterface.cs
@@ -0,0 +1,10 @@
+﻿using codemy_backend.Models.Dtos;
+using codemy_backend.Models.Dtos.Request;
+using codemy_backend.Models.Dtos.Response;
+
+namespace codemy_backend.Services.Auth;
+
+public interface IAuthInterface
+{
+    Task<ResponseModel> SignUp(SignUpRequestDto signUpRequestDto);
+}
