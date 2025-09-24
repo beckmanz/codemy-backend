86ecf429b31c2dbad2427ee92916d2d0e69c64f7
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-24 13:06:58 -0300

    feat: implementa repositoriy para User, e cria metodos CreateUserAsync e GetByEmailAsync

---
+++ codemy-backend/Repositories/User/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using codemy_backend.Models.Dtos;
+
+namespace codemy_backend.Repositories.User;
+
+public interface IUserRepository
+{
+    Task<UserModel> CreateUserAsync(UserModel userModel);
+    Task<UserModel> GetByEmailAsync(string email);
+}
---
+++ codemy-backend/Repositories/User/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace codemy_backend.Repositories.User;
+
+public class UserRepository : IUserRepository
+{   
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<UserModel> CreateUserAsync(UserModel userModel)
+    {
+        var user = await _context.Users.AddAsync(userModel);
+        await _context.SaveChangesAsync();
+        return user.Entity;
+    }
+
+    public async Task<UserModel> GetByEmailAsync(string email)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(x => x.Email == email);
+        return user;
+    }
+}
