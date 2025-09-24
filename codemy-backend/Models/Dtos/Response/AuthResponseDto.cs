948ab256db320ee6201f9b9079bc9f3521313482
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-24 13:51:44 -0300

    feat: cria AuthResponseDto para respostas de requisições de authenticação

---
+++ codemy-backend/Models/Dtos/Response/AuthResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace codemy_backend.Models.Dtos.Response;
+
+public class AuthResponseDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Token { get; set; }
+    public static implicit operator AuthResponseDto(UserModel dto)
+    {
+        return new AuthResponseDto()
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+        };
+    }
+}
