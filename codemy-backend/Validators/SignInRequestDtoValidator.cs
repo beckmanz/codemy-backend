441831cae811e1756840b6d4214ba54fa67d778e
Mauricio Santos <mauriciosantosnetto@gmail.com>
2025-09-27 17:20:49 -0300

    feat: cria SignInRequestDtoValidator para validar os dados de login do usuário

---
+++ codemy-backend/Validators/SignInRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using codemy_backend.Models.Dtos.Request;
+using FluentValidation;
+
+namespace codemy_backend.Validators;
+
+public class SignInRequestDtoValidator: AbstractValidator<SignInRequestDto>
+{
+    public SignInRequestDtoValidator()
+    {
+        RuleFor(s => s.Email)
+            .NotEmpty().WithMessage("Email é obrigatório!")
+            .EmailAddress().WithMessage("Email inválido!");
+
+        RuleFor(s => s.Password)
+            .NotEmpty().WithMessage("Senha é obrigatória!");
+    }
+}
